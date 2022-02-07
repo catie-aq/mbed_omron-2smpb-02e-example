b004d06d3bba97efc3cd37745225e0abc01c746c
Clément Laigle <c.laigle@catie.fr>
2022-02-07 10:09:55 +0100

    Add example

--- main.cpp
+++ main.cpp
@@ -3,20 +3,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
+#include "2SMPB02E.h"
+
+using namespace sixtron;
 
 namespace {
-#define HALF_PERIOD 500ms
+#define HALF_PERIOD 2s
 }
 
 static DigitalOut led1(LED1);
 
+O2SMPB02E smp(I2C1_SDA, I2C1_SCL);
+
 int main()
 {
+    if(!smp.init()){
+        printf("Failed to init 2SMPB02E\n");
+    }
     while (true) {
         led1 = !led1;
-        if (led1) {
-            printf("Alive!\n");
-        }
+        smp.temperature();
+        // if (led1) {
+        //     printf("Alive!\n");
+        // }
         ThisThread::sleep_for(HALF_PERIOD);
     }
 }
