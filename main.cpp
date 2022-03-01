decb01f2939e1d75e72fd512e6b788c65dd1b211
Clément Laigle <c.laigle@catie.fr>
2022-03-01 21:07:51 +0100

    Update application

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
  * Copyright (c) 2022, CATIE
  * SPDX-License-Identifier: Apache-2.0
  */
-#include "mbed.h"
 #include "2SMPB02E.h"
+#include "mbed.h"
 
 using namespace sixtron;
 
 namespace {
-#define HALF_PERIOD 2s
+#define PERIOD 1s
 }
 
 static DigitalOut led1(LED1);
@@ -17,15 +17,25 @@ O2SMPB02E smp(I2C1_SDA, I2C1_SCL);
 
 int main()
 {
-    if(!smp.init()){
+    if (!smp.init()) {
         printf("Failed to init 2SMPB02E\n");
     }
+
+    // Single Measurement
+    smp.measure_single_shot();
+
+    ThisThread::sleep_for(100ms);
+
+    printf("Temperature: %f °C\n", smp.temperature() / 256.0);
+    printf("Pressure: %f hPa\n\n", smp.pressure() / 100.0);
+
+    // Periodic Measurement
+    smp.start_periodic_measurement();
+
     while (true) {
         led1 = !led1;
-        smp.temperature();
-        // if (led1) {
-        //     printf("Alive!\n");
-        // }
-        ThisThread::sleep_for(HALF_PERIOD);
+        printf("Temperature: %f °C\n", smp.temperature() / 256.0);
+        printf("Pressure: %f hPa\n\n", smp.pressure() / 100.0);
+        ThisThread::sleep_for(PERIOD);
     }
 }
